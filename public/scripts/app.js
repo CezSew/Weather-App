9f65b4891e2af32186622434e4ae68fc27c89390
CezSew <csewerynski@interia.pl>
2018-08-22 22:11:18 +0200

    Added more specific translations

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -72,18 +72,36 @@ var Main = function (_React$Component) {
                 return "Bezchmurnie";
             } else if (weather === 'few clouds') {
                 return "Niskie zachmurzenie";
-            } else if (weather === 'scattered clouds' || weather === 'broken clouds') {
+            } else if (weather === 'scattered clouds' || weather === 'broken clouds' || weather === 'overcast clouds') {
                 return "Umiarkowane zachmurzenie";
             } else if (weather === 'mist') {
                 return "Mgła";
-            } else if (weather === 'shower rain') {
+            } else if (weather === 'shower rain' || weather === 'very heavy rain' || weather === 'heavy intensity shower rain') {
                 return "Ulewny deszcz";
-            } else if (weather === 'rain') {
+            } else if (weather === 'rain' || weather === 'light intensity shower rain' || weather === 'ragged shower rain') {
                 return "Deszcz";
+            } else if (weather === 'light rain') {
+                return "Mżawka";
+            } else if (weather === 'moderate rain') {
+                return "Umiarkowane opady deszczu";
+            } else if (weather === 'heavy intensity rain') {
+                return "Intensywne opady deszczu";
+            } else if (weather === 'extreme rain') {
+                return "Ekstremalne opady deszczu";
+            } else if (weather === 'freezing rain') {
+                return "Deszcz marznący";
             } else if (weather === 'thunderstorm') {
                 return "Burza z piorunami";
             } else if (weather === 'snow') {
-                return "Śnieg";
+                return "Opady śniegu";
+            } else if (weather === 'light snow') {
+                return "Małe opady śniegu";
+            } else if (weather === 'heavy snow') {
+                return "Duże opady śniegu";
+            } else if (weather === 'sleet' || weather === 'shower sleet' || weather === 'light rain and snow' || weather === 'rain and snow' || weather === 'light shower snow' || weather === 'shower snow' || weather === 'heavy shower snow') {
+                return "Śnieg z deszczem";
+            } else if (weather === 'sleet') {
+                return "Śnieg z deszczem";
             } else {
                 return weather;
             }
