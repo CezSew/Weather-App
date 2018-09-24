4bf2777825441f22110fa2e94d9fdbd8fab22b99
CezSew <csewerynski@interia.pl>
2018-09-24 22:27:21 +0200

    Created function getting weather for three days ahead

--- src/Main.js
+++ src/Main.js
@@ -19,9 +19,15 @@ class Main extends React.Component {
             current_temperature: '',
             current_pressure: '',
             current_weather: '',
-            nextDay_temperature: '',
-            nextDay_pressure: '',
-            nextDay_weather: ''
+            day_2_temperature: '',
+            day_2_pressure: '',
+            day_2_weather: '',
+            day_3_temperature: '',
+            day_3_pressure: '',
+            day_3_weather: '',
+            day_4_temperature: '',
+            day_4_pressure: '',
+            day_4_weather: ''
         };
     }
 
@@ -48,9 +54,9 @@ class Main extends React.Component {
         .then(data => APICallbackObject = data)
         .then(() => {
             const currentWeather = this.getCurrentWeather(APICallbackObject);
-            const nextDayWeather = this.getNextDay(APICallbackObject);
+            const nextDaysWeather = this.getThreeDays(APICallbackObject);
             const country = APICallbackObject.city.country; 
-            this.setApplicationState(currentWeather, nextDayWeather, city, country);
+            this.setApplicationState(currentWeather, nextDaysWeather, city, country);
             console.log(APICallbackObject);
         }).catch(error => {
             this.setState({error: true, typedCity: city});
@@ -64,10 +70,22 @@ class Main extends React.Component {
         return {temperature, pressure, weather};
     }
 
-    getNextDay(callback) {
-        const temperature = this.convertToCelsius(callback.list[8].main.temp);
-        const pressure = callback.list[8].main.pressure;
-        const weather = callback.list[8].weather[0].description;
+    getThreeDays(callback) {
+        const temperature = [
+            this.convertToCelsius(callback.list[8].main.temp),
+            this.convertToCelsius(callback.list[16].main.temp),
+            this.convertToCelsius(callback.list[24].main.temp),
+        ];
+        const pressure = [
+            callback.list[8].main.pressure,
+            callback.list[16].main.pressure,
+            callback.list[24].main.pressure,
+        ];
+        const weather = [
+            callback.list[8].weather[0].description,
+            callback.list[16].weather[0].description,
+            callback.list[24].weather[0].description
+        ];
         return {temperature, pressure, weather};
     }
 
@@ -93,7 +111,7 @@ class Main extends React.Component {
         }
     }
 
-    setApplicationState(currentWeather, nextDayWeather, city, country) {
+    setApplicationState(currentWeather, nextDaysWeather, city, country) {
         this.setState({
             error: false,
             typedCity: city,
@@ -102,9 +120,15 @@ class Main extends React.Component {
             current_temperature: currentWeather.temperature + ' ℃',
             current_pressure: currentWeather.pressure + ' hPa',
             current_weather: currentWeather.weather,
-            nextDay_temperature: nextDayWeather.temperature + ' ℃',
-            nextDay_pressure: nextDayWeather.pressure + ' hPa',
-            nextDay_weather: nextDayWeather.weather
+            day_2_temperature: nextDaysWeather.temperature[0] + ' ℃',
+            day_2_pressure: nextDaysWeather.pressure[0] + ' hPa',
+            day_2_weather: nextDaysWeather.weather[0],
+            day_3_temperature: nextDaysWeather.temperature[1] + ' ℃',
+            day_3_pressure: nextDaysWeather.pressure[1] + ' hPa',
+            day_3_weather: nextDaysWeather.weather[1],
+            day_4_temperature: nextDaysWeather.temperature[2] + ' ℃',
+            day_4_pressure: nextDaysWeather.pressure[2] + ' hPa',
+            day_4_weather: nextDaysWeather.weather[2],
         });
     }
     /*
@@ -131,11 +155,20 @@ class Main extends React.Component {
                     country={this.state.country} 
                     typedCity={this.state.typedCity} 
                     pressure={this.state.current_pressure} 
-                    nextDay_pressure={this.state.nextDay_pressure}
                     weather={this.state.current_weather} 
                     temperature={this.state.current_temperature} 
                     isError={this.state.error}
-                    animateDataBoxes={this.animateDataBoxes} />
+                    animateDataBoxes={this.animateDataBoxes} 
+                    day_2_pressure={this.state.day_2_pressure}
+                    day_2_temperature={this.state.day_2_temperature}
+                    day_2_weather={this.state.day_2_weather}
+                    day_3_pressure={this.state.day_3_pressure}
+                    day_3_temperature={this.state.day_3_temperature}
+                    day_3_weather={this.state.day_3_weather}
+                    day_4_pressure={this.state.day_4_pressure}
+                    day_4_temperature={this.state.day_4_temperature}
+                    day_4_weather={this.state.day_4_weather}
+                    />
                 </div>
                 <Footer />
             </div>
